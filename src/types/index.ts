ab071ad14d54aec673d785c09581aa0819ef0264
senlin <gougewh@sina.com>
2021-01-29 09:51:21 +0800

    feat: 响应data
    编写对应的返回数据响应body的功能

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,7 @@ export interface AxiosRequestConfig {
   data?: any
   params?: any
   headers?: any
+  responseType?: XMLHttpRequestResponseType
 }
 
 export type Method =
@@ -21,3 +22,16 @@ export type Method =
   | 'PUT'
   | 'patch'
   | 'PATCH'
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
+
+export type XMLHttpRequestResponseType = '' | 'arraybuffer' | 'blob' | 'document' | 'json' | 'text'
